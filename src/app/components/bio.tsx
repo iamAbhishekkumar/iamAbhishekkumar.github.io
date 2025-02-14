9974575335d90b6c69f17fd524480aa7e02f3758
iamAbhishekkumar <abhisonuk123@gmail.com>
2025-02-14 22:42:57 +0530

    Added Bio

---
+++ src/app/components/bio.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function Bio() {
+  return (
+    <div className="space-y-2 mb-8">
+      <p>
+        • backend engineer based in <span className="text-white">india</span>
+      </p>
+      <p>
+        • worked with{" "}
+        <Link
+          href="https://www.linkedin.com/in/abhishek-kumar-531512176/details/experience/"
+          className="text-white hover:underline"
+        >
+          few startups
+        </Link>
+        , currently{" "}
+        <Link
+          href="https://www.linkedin.com/company/tataelxsi"
+          className="text-white hover:underline"
+        >
+          @Tata Elxsi
+        </Link>
+      </p>
+      <p>
+        • currently building things from scratch to learn.{" "}
+        <Link
+          href="https://github.com/iamAbhishekkumar/gitlite"
+          className="text-white hover:underline"
+        >
+          Check out GitLite
+        </Link>
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Header from "@/app/components/header";
+import Bio from "@/app/components/bio";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-gray-300 p-8 max-w-4xl mx-auto">
       <Header />
+      <Bio />
+      {/* TODO : 
+      1. Added GH Page Configurations
+      2. Added Starting Animation of Fade In
+      3. Change the font
+       */}
     </div>
   );
 }
